aa8e24a06a0d0d128668d3e0a1cb1cbd194beed9
chiofus <132926798+chiofus@users.noreply.github.com>
2026-03-28 08:29:57 -0700

    redid reqs

--- tools/empty_parcels.py
+++
@@ -1,6 +0,0 @@
-#This contains the code for returning where there exist points (representing civic addresses) that do not belong to a building parcel (ie they must be empty)
-
-#General objects
-# ALL_CIVIC_POINTS_QUERY: str = ''
-
-# pass
